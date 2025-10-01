42a3bbbc5ad10a3b56a7ed315cbdf88883bcf399
boibyy <boibyboiby3@gmail.com>
2025-10-01 16:00:27 +1000

    moderation portal

---
+++ translations-en.js
@@ -0,0 +1,102 @@
+// translations-en.js
+window.translations = {
+    en: {
+
+        "Title.Warn": "Warning",
+        "Title.Ban1Hour": "Banned for 1 Hour",
+        "Title.Ban3Hours": "Banned for 3 Hours",
+        "Title.Ban6Hours": "Banned for 6 Hours",
+        "Title.Ban12Hours": "Banned for 12 Hours",
+        "Title.Ban1Day": "Banned for 1 Day",
+        "Title.Ban2Days": "Banned for 2 Days",
+        "Title.Ban3Days": "Banned for 3 Days",
+        "Title.Ban7Days": "Banned for 7 Days",
+        "Title.Ban14Days": "Banned for 14 Days",
+        "Title.Ban30Days": "Banned for 30 Days",
+        "Title.Ban60Days": "Banned for 60 Days",
+        "Title.Ban6Months": "Banned for 6 Months",
+        "Title.Ban1Year": "Banned for 1 Year",
+        "Title.Delete": "Account Deleted",
+        "Title.Terminate": "Account Terminated",
+        "Title.AccountBanned": "Account Banned",
+      
+        "Description.Warn": "You may re-activate your account by agreeing to our Terms of Use.",
+        "Description.Ban": "Your account has been disabled. You may re-activate it after {0}.",
+        "Description.BanV2": "Your account was suspended due to a recent violation. You may re-activate it after {0}.",
+        "Description.Delete": "Your account has been terminated.",
+      
+        "Text.TermsMessage": "We have determined that your behaviour or actions on Boiby have been in violation of our {0}.",
+        "Text.CommunityStandardMessage": "Please abide by the {0} so that Boiby can be a safe and enjoyable place for everyone.",
+        "Text.AppealsMessage": "To request an appeal visit the {0}.",
+        "Text.DidWeMistake": "Did we make a mistake? {0}.",
+        
+        "Label.TermsOfUse": "Terms of Use",
+        "Label.CommunityRules": "Boiby Community Rules",
+        "Label.AppealsForm": "Appeals Form",
+        "Label.Reviewed": "Reviewed",
+        "Label.ModeratorNote": "Moderator Note",
+        "Label.Decision": "Decision",
+        "Label.Reason": "Reason",
+        "Label.OffensiveItem": "Offensive Item",
+        "Label.RecentViolation": "Recent Violation",
+        
+        "Text.ParentReactivate": "Your parent or guardian must help you re-activate this account.",
+
+        "Label.Here": "Let us know",
+        
+        "Button.Logout": "Logout",
+        "Button.Reactivate": "Re-Activate Account",
+        "Button.Parent": "I'm a Parent or Guardian",
+
+        // TODO: Deprecation of legacy caution warnings to streamline existing system (do this after caution warning logic is removed)
+        "Caution.Title": "CAUTION",
+        "Caution.Future": "If you violate the Community Rules again, your account may be suspended in the future.",
+        "Caution.1Day": "If you violate the Community Rules again, your account may be suspended for at least 1 day.",
+        "Caution.3Days": "If you violate the Community Rules again, your account may be suspended for at least 3 days.",
+        "Caution.7Days": "If you violate the Community Rules again, your account may be suspended for at least 7 days.",
+        "Caution.Permanent": "If you violate the Community Rules again, your Boiby account may be permanently banned.",
+        // Note to translators: You don't have to translate the 6 lines above. Just leave them as English as they will be removed anyway.
+        
+        "Decision.ManualDetectionAutoReview": "We used automated systems when making this decision.",
+        "Decision.AutoDetectionManualReview": "We used automated systems to detect this violation.",
+        "Decision.FullyAutomated": "We used automated systems to detect this violation and make this decision.",
+
+        "Heading.Warn": "Warning",
+        "Heading.Suspend": "Suspended",
+        "Heading.Suspend1Hour": "Suspended for 1 hour",
+        "Heading.Suspend3Hours": "Suspended for 3 hours",
+        "Heading.Suspend6Hours": "Suspended for 6 hours",
+        "Heading.Suspend12Hours": "Suspended for 12 hours",
+        "Heading.Suspend1Day": "Suspended for 1 day",
+        "Heading.Suspend2Days": "Suspended for 2 days",
+        "Heading.Suspend3Days": "Suspended for 3 days",
+        "Heading.Suspend7Days": "Suspended for 7 days",
+        "Heading.Suspend14Days": "Suspended for 14 days",
+        "Heading.Suspend30Days": "Suspended for 30 days",
+        "Heading.Suspend60Days": "Suspended for 60 days",
+        "Heading.Suspend6Months": "Suspended for 6 months",
+        "Heading.Suspend1Year": "Suspended for 1 year",
+        "Heading.Banned": "Banned",
+
+        "Label.LatestActivity": "Latest activity we reviewed",
+        "Label.ModeratorNoteV2": "Moderator note",
+        "Label.ReviewDateV2": "Review date",
+        "Label.DecisionV2": "Decision method",
+        "Label.OffensiveItemV2": "Offensive item",
+        "Label.AssetID": "Asset ID",
+        "Label.BundleName": "Bundle Name",
+        "Label.BundleID": "Bundle ID",
+        "Label.ModelName": "Model Name",
+        "Label.AccessoryName": "Accessory Name",
+        "Label.AccessoryID": "Accessory ID",
+        "Label.Location": "Location",
+        "Label.ProfilePicture": "Profile Picture",
+        "Description.FollowTheRules": "Make sure you know and follow the rules. {0}",
+
+        "Action.FollowRulesLink": "View rules",
+        "Action.Logout": "Logout",
+        "Action.Continue": "Continue",
+        "Action.ReportAMistake": "Report a mistake",
+        "Action.Back": "Back"
+    }
+};
